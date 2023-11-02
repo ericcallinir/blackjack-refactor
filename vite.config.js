10895bd00fd356e30ad85e6f975b3abb890010c4
Eric Isaac Callinir Lizama <eric.callinir@scotiabank.cl>
2023-11-02 15:37:15 -0300

    vite.config added

---
+++ vite.config.js
@@ -0,0 +1,5 @@
+import { defineConfig} from 'vite';
+
+export default defineConfig({
+    base: '/blackjack-refactor/',//Aqui le estamos dando la ruta del repositorio de github para que lo haga automatico y no tengamos que hacerlo en el index.html de forma manual.
+})
